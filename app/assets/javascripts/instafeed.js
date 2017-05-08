45b79d22fc06f0eb29884116ddc8de1d369c1616
John Talio <johntalio@yahoo.com>
2017-05-08 12:26:12 -0400

    Attempt to add instagram feed with instafeed gem

---
+++ app/assets/javascripts/instafeed.js
@@ -0,0 +1,12 @@
+function Instafeed() {
+  get: 'user',
+  userId: 260898138,
+  target: 'Instafeed',
+  sortBy: 'most-recent',
+  links: true,
+  limit: 10,
+  resolution: 'thumbnail',
+  clientId: '717963a622184317a84876ab7f7df18d'
+};
+
+Instafeed.run();
