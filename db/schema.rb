6f9e76455e7fae74298c1273c291e6dcfa1fbe43
John Talio <johntalio@yahoo.com>
2017-05-08 12:25:51 -0400

    Tests with InstApi model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170508151846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "inst_apis", force: :cascade do |t|
+  end
 
 end
